04c9e05972147c906a87ce18a98dcd04f6b95854
bukanekosoed <ekosudarwanto98@gmail.com>
2023-12-16 19:45:44 +0700

    ---getus

--- app.py
+++ app.py
@@ -452,5 +452,26 @@ def get_all_data():
         return jsonify({'message': 'Token tidak valid!'})
    
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/admin/detail/users')
+def detail_users():
+    informasi = get_user_data()
+    return render_template('admin/user.html',informasi=informasi, active_page="detail_users")
+
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
